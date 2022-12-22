dc18c9a0682448bcef19c66009740b046105443f
TeNg <tenm@urbox.vn>
2022-12-22 11:26:53 +0700

    update client module

--- client_module.go
+++ client_module.go
@@ -10,7 +10,7 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-type ISGrpcClient interface {
+type IClient interface {
 	UnaryClientInterceptor(dialConfig *DialConfig, dialOpts ...grpc.DialOption) grpc.UnaryClientInterceptor
 	GetShardCount() int
 }
