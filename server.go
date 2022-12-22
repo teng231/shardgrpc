70cdd1276be30164ef0edb4f887a46b9df7cc7a7
TeNg <tenm@urbox.vn>
2022-12-22 10:48:39 +0700

    add get shard index

--- server.go
+++ server.go
@@ -30,6 +30,21 @@ func GetServerShardKey(ctx context.Context, message interface{}) string {
 	return msgrefl.Get(defShardKey).String()
 }
 
+func GetShardIndex(hostname string) int {
+	if hostname == "" {
+		hostname = os.Getenv("HOSTNAME")
+	}
+	arr := strings.Split(hostname, "-")
+	if len(arr) < 2 {
+		log.Panicf("hostname '%s' not valid form xxx-i", hostname)
+	}
+	index, err := strconv.Atoi(arr[len(arr)-1])
+	if err != nil {
+		log.Panicf("hostname not include index, err: %s", err.Error())
+	}
+	return index
+}
+
 // UnaryServerInterceptorStatefullset is called on every request received from a client to a
 // unary server operation, here, we pull out the client operating system from
 // the metadata, and inspect the context to receive the IP address that the
---
+++ server_test.go
@@ -0,0 +1,18 @@
+package shardgrpc
+
+import "testing"
+
+func TestShardIndex(t *testing.T) {
+	cases := map[string]int{
+		"api-5":             5,
+		"a12-getcode-3":     3,
+		"a12-getcode-abc-4": 4,
+		// "a19-getlink-XDS32-1s3r": 0,
+	}
+	for host, index := range cases {
+		i := GetShardIndex(host)
+		if i != index {
+			t.Fail()
+		}
+	}
+}
