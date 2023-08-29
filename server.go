b53e9204e516cbd292b31e60798a05737b817bee
TeNg <tenm@urbox.vn>
2023-08-29 17:25:31 +0700

    add: get shard index

--- server.go
+++ server.go
@@ -34,6 +34,21 @@ type ServerInfo struct {
 	Port int
 }
 
+func GetShardIndex(hostname string) int {
+	if hostname == "" {
+		hostname = os.Getenv("HOSTNAME")
+	}
+	arr := strings.Split(hostname, "-")
+	if len(arr) < 2 {
+		log.Panicf("hostname '%s' not valid form xxx-i", hostname)
+	}
+	index, err := strconv.Atoi(arr[len(arr)-1])
+	if err != nil {
+		log.Panicf("hostname not include index, err: %s", err.Error())
+	}
+	return index
+}
+
 // UnaryServerInterceptorPreset is called on every request received from a client to a
 // unary server operation, here, we pull out the client operating system from
 // the metadata, and inspect the context to receive the IP address that the
